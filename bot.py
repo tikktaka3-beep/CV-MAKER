64c1fc80a7bf4086abae3344c34a134a4627346d
tikktaka3-beep <tikktaka3@gmail.com>
2026-06-24 23:31:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher   # ✅ To‘g‘ri import
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import BotCommand
