605f46c789d9e00dc99ed471df5e9fc0cfc58068
tikktaka3-beep <tikktaka3@gmail.com>
2026-06-26 08:39:07 +0500

    Delete image_processor.py

--- image_processor.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-def process_photo(photo_path: str, output_path: str):
-    img = Image.open(photo_path)
-    img = img.convert("RGB")
-    img.thumbnail((300, 300))
-    img.save(output_path)
-    return output_path
